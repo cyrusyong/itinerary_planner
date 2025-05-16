a48fcce93856acb310f8d5097333778c660ee9c1
Cyrus Yong <cyrusyong1@gmail.com>
2025-05-16 18:36:24 -0400

    add routing capabilities

--- itinerary_planner/src/main.jsx
+++ itinerary_planner/src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router'
 import './index.css'
 import Home from './Home.jsx'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>
+  },
+])
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Home />
-  </StrictMode>,
+  <RouterProvider router={router}/>
 )
