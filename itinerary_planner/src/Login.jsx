489810b95c267f821df61aafec74d29bca74d7f6
Cyrus Yong <cyrusyong1@gmail.com>
2025-05-16 18:50:21 -0400

    feature: add routing capabilities between Home and Login

--- itinerary_planner/src/Login.jsx
+++ itinerary_planner/src/Login.jsx
@@ -1,19 +1,23 @@
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import './Login.css'
 
 function Login() {
   const [count, setCount] = useState(0)
+  const navigate = useNavigate()
 
   return (
     <>
       <h1>Login</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => {
+          navigate("/")
+        }}>
+          Go back to Home
         </button>
       </div>
     </>
   )
 }
 
-export default Home
+export default Login
--- itinerary_planner/src/main.jsx
+++ itinerary_planner/src/main.jsx
@@ -1,16 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import Home from './Home.jsx'
+import Login from './Login.jsx'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home/>
+    element: <Home />
   },
+  {
+    path: "/login",
+    element: <Login />
+  }
 ])
 
 createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router}/>
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
 )
