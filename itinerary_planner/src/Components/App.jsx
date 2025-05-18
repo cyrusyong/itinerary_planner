f0dfda4c23476312bdd657698f3e807619b9d696
Cyrus Yong <cyrusyong1@gmail.com>
2025-05-18 01:45:15 -0400

    add google maps

--- itinerary_planner/src/Components/App.jsx
+++ itinerary_planner/src/Components/App.jsx
@@ -1,15 +1,55 @@
-import { Link } from "react-router-dom";
+import { Loader } from "@googlemaps/js-api-loader"
 import NavBar from "./Navbar";
+import { useRef, useEffect } from 'react'
 
 function App() {
-    return (
-      <>
-        <NavBar />
-        <div>
-            This is the app component
-        </div>
-      </>
-    )
+  const mapRef = useRef(null);
+  const mapElement = useRef(null);
+
+  useEffect(() => {
+    const loader = new Loader({
+      apiKey: "AIzaSyCGJ6bRHMUhb0oYGrxFimZ5CPNRZd1qSoU",
+      version: "weekly"
+    });
+
+    loader.load().then(async () => {
+      const { ColorScheme } = await google.maps.importLibrary("core")
+      const { Map } = await google.maps.importLibrary("maps");
+
+      const map = new Map(mapElement.current, {
+        center: { lat: 35.6764, lng: 139.65 },
+        zoom: 6,
+        colorScheme: ColorScheme.DARK,
+      });
+
+      mapRef.current = map;
+    })
+  }, [])
+
+  const handleGoToLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+
+        mapRef.current.setCenter(pos)
+        mapRef.current.setZoom(16)
+      })
+    }
+  }
+
+  return (
+    <>
+      <NavBar />
+      <div>
+        This is the app component
+      </div>
+      <div id="maps" ref={mapElement}></div>
+      <button id="current-location-button" onClick={handleGoToLocation}>Go to my location</button>
+    </>
+  )
 }
 
 export default App
