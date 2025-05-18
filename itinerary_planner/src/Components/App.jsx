cef37e6341a3c43a609423525eae1e872149779d
xurichard19 <xurichard19@gmail.com>
2025-05-18 01:42:51 -0400

    added footer and bg

--- itinerary_planner/src/Components/App.jsx
+++ itinerary_planner/src/Components/App.jsx
@@ -1,13 +1,17 @@
 import { Link } from "react-router-dom";
 import NavBar from "./Navbar";
+import Footer from "./Footer";
 
 function App() {
     return (
       <>
         <NavBar />
+
         <div>
             This is the app component
         </div>
+
+        <Footer />
       </>
     )
 }
