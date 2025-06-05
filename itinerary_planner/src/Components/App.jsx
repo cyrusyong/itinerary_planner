9640fa6e11c5a4c64e39bb841f62f87d3d116766
Cyrus Yong <cyrusyong1@gmail.com>
2025-06-05 01:26:01 -0400

    reorganize CSS into respective files for App and Placecard

--- itinerary_planner/src/Components/App.jsx
+++ itinerary_planner/src/Components/App.jsx
@@ -1,6 +1,7 @@
 import { Loader } from "@googlemaps/js-api-loader"
 import { useRef, useEffect, useState } from 'react'
-import PlaceCard  from './Placecard.jsx'
+import PlaceCard from './Placecard.jsx'
+import styles from '../Styles/App.module.css'
 
 function App() {
   const mapRef = useRef(null);
@@ -54,7 +55,39 @@ function App() {
       region: 'jp',
     }
 
-    const { places } = await Place.searchByText(request);
+    // const { places } = await Place.searchByText(request);
+    const places = [
+      {
+        "id": "ChIJq_fYt4iLGGARrOojmQ4IMyE",
+        "displayName": "Starbucks Reserve Roastery Tokyo",
+        "location": {
+          "lat": 35.6492642,
+          "lng": 139.69259069999998
+        },
+        "formattedAddress": "2-chōme-19-23 Aobadai, Meguro City, Tokyo 153-0042",
+        "rating": 4.5
+      },
+      {
+        "id": "ChIJL0MSsuaLGGARFMGsCD4gT_0",
+        "displayName": "Starbucks Reserve® Store - Ginza Marronnier-dori Street",
+        "location": {
+          "lat": 35.6722648,
+          "lng": 139.7675641
+        },
+        "formattedAddress": "〒104-0061 Tokyo, Chuo City, Ginza, 3-chōme−7−３ Ginza Omi Bldg., 1F",
+        "rating": 4
+      },
+      {
+        "id": "ChIJ30G_l3iLGGARYNilOEO6xVI",
+        "displayName": "Starbucks Reserve - Tokyo Midtown",
+        "location": {
+          "lat": 35.6650037,
+          "lng": 139.7307146
+        },
+        "formattedAddress": "〒107-0052 Tokyo, Minato City, Akasaka, 9-chōme−7−２ 東京ミッドタウンB0103",
+        "rating": 4
+      }
+    ]
     if (places.length) {
       const formattedPlaces = []
       console.log(places)
@@ -92,25 +125,31 @@ function App() {
 
   return (
     <>
-      <div className="app-container">
-        <div id="list">
-          {places.length == 0 && <h2>Where are we headed off to?</h2>}
-          <form action="" onSubmit={(e) => {
-            e.preventDefault()
-            handleSearch()
-          }}>
-            <input type="text" id="place-search" value={query} onChange={(e) => setQuery(e.target.value)} />
-          </form>
+      <div className={styles.appContainer}>
+        <div>
+          <div id={styles.list}>
+            {places.length == 0 && <h2>Where are we headed off to?</h2>}
+            <form action="" onSubmit={(e) => {
+              e.preventDefault()
+              handleSearch()
+            }}>
+              <input type="text" id={styles.placeSearch} value={query} onChange={(e) => setQuery(e.target.value)} />
+            </form>
 
-          {places.length > 0 && (
-            <div className="card-list">
-              {places.map((place, index) => (
-                <PlaceCard key={index} place={place} />
-              ))}
-            </div>
-          )}
+            {places.length > 0 && (
+              <div className={styles.cardList}>
+                {places.map((place, index) => (
+                  <PlaceCard key={index} place={place} />
+                ))}
+              </div>
+            )}
+          </div>
+          <div>
+            
+          </div>
         </div>
-        <div id="maps" ref={mapElement} />
+
+        <div id={styles.maps} ref={mapElement} />
       </div>
 
       <button id="current-location-button" onClick={handleGoToLocation}>Go to my location</button>
--- itinerary_planner/src/Components/Placecard.jsx
+++ itinerary_planner/src/Components/Placecard.jsx
@@ -1,9 +1,15 @@
+import styles from '../Styles/Placecard.module.css'
+
 function PlaceCard({ place }) {
     return (
-        <div className="place-card">
+        <div className={styles.placeCard}>
             <h3>{place.name}</h3>
             <p>{place.address}</p>
             <p>{place.rating} Stars</p>
+            <div className={styles.placeButtonContainer}>
+                <button id={styles.placeRemoveButton}>-</button>
+                <button id={styles.placeAddButton}>+</button>
+            </div>
         </div>
     )
 }
