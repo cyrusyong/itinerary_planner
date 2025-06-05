bd324a71b53ce7288c48de9525b8f255b43f1b45
Cyrus Yong <cyrusyong1@gmail.com>
2025-06-05 03:04:39 -0400

    renders an add/remove button based on where card is going to be placed

--- itinerary_planner/src/Components/Placecard.jsx
+++ itinerary_planner/src/Components/Placecard.jsx
@@ -1,14 +1,20 @@
 import styles from '../Styles/Placecard.module.css'
 
-function PlaceCard({ place }) {
+function PlaceCard({ place, onAdd, onRemove, isList }) {
     return (
         <div className={styles.placeCard}>
             <h3>{place.name}</h3>
             <p>{place.address}</p>
             <p>{place.rating} Stars</p>
             <div className={styles.placeButtonContainer}>
-                <button id={styles.placeRemoveButton}>-</button>
-                <button id={styles.placeAddButton}>+</button>
+                {isList ?
+                    <button id={styles.placeRemoveButton} onClick={() => {
+                        onRemove(place.id)
+                    }}>-</button>
+                    :
+                    <button id={styles.placeAddButton} onClick={() => {
+                        onAdd(place.id)
+                    }}>+</button>}
             </div>
         </div>
     )
