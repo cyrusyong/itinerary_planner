2ca0a67f9d996f3216416d8d076e33d192713bd9
Cyrus Yong <cyrusyong1@gmail.com>
2025-06-21 00:33:32 -0400

    refactor homepage with new color scheme and animated components

--- itinerary_planner/src/Components/Home.jsx
+++ itinerary_planner/src/Components/Home.jsx
@@ -1,18 +1,75 @@
 import '../Styles/index.css'
 import { Link } from 'react-router-dom'
+import SpotlightCard from './SpotlightCard/SpotlightCard.jsx'
+import SplitText from "./SplitText/SplitText.jsx"
+import styles from '../Styles/Home.module.css'
+import CardSwap, { Card } from "./CardSwap/CardSwap.jsx"
+import RotatingText from "./RotatingText/RotatingText.jsx"
 
 function Home() {
 
   return (
     <>
-      <div className='container'>
-        <h1>Welcome to Shepherd</h1>
+      <div className={styles.container}>
+        <div className={styles.textContainer}>
+          <div className={styles.textTopContainer}>
+            <SplitText
+              text="Your Trip to "
+              className={styles.splitText}
+              delay={50}
+              duration={0.6}
+              ease="power3.out"
+            />
 
-        <Link to={"./app"}>
-          <button>
-            Launch app
+            <RotatingText
+              texts={["NC", "NYC", "Japan"]}
+              rotationInterval={3500}
+              staggerFrom={"first"}
+              className={styles.rotatingText}
+            />
+          </div>
+          <SplitText
+            text="Expertly Guided"
+            className={styles.splitText}
+            delay={50}
+            duration={0.6}
+            ease="power3.out"
+          />
+
+          <Link to={"/app"}>
+          <button className={styles.spotlightButton}>
+            <SpotlightCard spotlightColor='rgb(229, 229, 229)' className={styles.spotlightCard}>
+            <h3 className={styles.launchText}>Launch App</h3>
+          </SpotlightCard>
           </button>
-        </Link>
+          </Link>
+
+
+        </div>
+
+        <div className={styles.cardSwapContainer}>
+          <CardSwap
+            cardDistance={60}
+            verticalDistance={120}
+            delay={3500}
+            pauseOnHover={false}
+            width={"35vw"}
+            height={"65vh"}
+          >
+            <Card>
+              <img src="../public/shrine.jpg" alt="shrine" className={styles.cardImage} />
+            </Card>
+
+            <Card>
+              <img src="../public/NC.jpg" className={styles.cardImage} />
+            </Card>
+
+            <Card>
+              <img src="../public/timesquare.jpg" alt="times Square" className={styles.cardImage} />
+            </Card>
+
+          </CardSwap>
+        </div>
 
       </div>
     </>
--- itinerary_planner/src/Components/Navbar.jsx
+++ itinerary_planner/src/Components/Navbar.jsx
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom'
 import '../Styles/index.css'
+import SpotlightCard from './SpotlightCard/SpotlightCard'
+import styles from "../Styles/Navbar.module.css"
 
 function NavBar() {
   return (
@@ -7,13 +9,21 @@ function NavBar() {
       <Link to={"/"}>
         <img src="/baby.svg" alt="Logo" className="logo" />
       </Link>
-      <div>
+      <div className={styles.buttonContainer}>
         <Link to={"/login"}>
-          <button>Login</button>
+          <button className={styles.spotlightButton}>
+            <SpotlightCard className={styles.spotlightCard}>
+              <h4 className={styles.spotlightText}>Login</h4>
+            </SpotlightCard>
+          </button>
         </Link>
 
         <Link to={"/register"}>
-          <button>Register</button>
+          <button className={styles.spotlightButton}>
+            <SpotlightCard className={styles.spotlightCard}>
+              <h4 className={styles.spotlightText}>Register</h4>
+            </SpotlightCard>
+          </button>
         </Link>
       </div>
     </nav>
