a6833770f27b6007654b622c2db84ff5556b1639
Cyrus Yong <cyrusyong1@gmail.com>
2025-07-31 19:36:36 -0400

    select tags along with appropriate data implemented

--- itinerary_planner/src/Components/CoreInput/TagSelection/TagSelection.jsx
+++ itinerary_planner/src/Components/CoreInput/TagSelection/TagSelection.jsx
@@ -1,21 +1,57 @@
 import { useEffect, useState } from "react";
 
 const TagSelection = ({ tags }) => {
-  const [tagsObj, setTagsObj] = useState({});
+  const [tagArray, setTagArray] = useState([])
+  const [selectedTags, setSelectedTags] = useState([])
+
   useEffect(() => {
     if (tags) {
-      setTagsObj(tags);
+      console.log(tags)
+      setTagArray(tags)
     }
-  }, [tags]);
+  }, [tags])
 
-  if (tagsObj) {
-    const tagsArray = Object.entries(tagsObj);
-    console.log(tagsArray)
+  function handleTagClick(e) {
+    const updatedTagArray = [...tagArray]
+
+    const [, tagData] = updatedTagArray[e.target.id]
+    console.log(tagData)
+
+    if (tagData.tagSelected) {
+      tagData.tagSelected = false
+    } else {
+      tagData.tagSelected = true
+    }
+    setTagArray(updatedTagArray)
   }
 
   return (
     <>
       <div>Hello this is tag selection</div>
+      {tagArray.map(([tagName, tagData], index) => {
+        if (!tagData.tagSelected) {
+          return (
+            <button key={index} id={index} onClick={(e) => {
+              handleTagClick(e)
+            }}>{tagName}</button>
+          )
+        }
+      })}
+
+      <div>
+        {tagArray.map(([tagName, tagData], index) => {
+          if (tagData.tagSelected) {
+            return (
+              <button key={index} id={index} onClick={(e) => {
+                handleTagClick(e)
+              }}>{tagName}</button>
+            )
+          }
+        })}
+      </div>
+
+
+      <button>Next</button>
     </>
   );
 };
